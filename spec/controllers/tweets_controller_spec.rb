40d4a9063ec525ba4040bc2cf16c2e538f7120a1
globalcoinbay <admin@globalcoinbay>
2020-05-20 01:11:48 +0530

    add controller spec

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -2,19 +2,17 @@ require 'rails_helper'
 
 
 RSpec.describe TweetsController, type: :controller do
-    describe "GET #index" do
+    describe "Validate the Tweet controller" do
+        describe "GET #index" do
 
-        login_user
-        before do
-            get :index
+            login_user
+            before do
+                get :index
+            end
+            
+            it "returns http success" do
+                expect(response).to have_http_status(:success)
+            end
         end
-        
-        it "returns http success" do
-            expect(response).to have_http_status(:success)
-        end
-    #   it "JSON body response contains expected recipe attributes" do
-    #     json_response = JSON.parse(response.body)
-    #     expect(hash_body.keys).to match_array([:id, :ingredients, :instructions])
-    #   end
     end
 end
